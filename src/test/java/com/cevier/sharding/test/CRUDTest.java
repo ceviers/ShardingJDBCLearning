477ca677293ae0f8b308ea39e822b5d64775e145
cevier.wei <cevier.wei@ideamake.cn>
2023-02-21 15:36:08 +0800

    rebase test 2

--- src/test/java/com/cevier/sharding/test/CRUDTest.java
+++ src/test/java/com/cevier/sharding/test/CRUDTest.java
@@ -44,4 +44,10 @@ public class CRUDTest {
         var paragraphs = paragraphService.getByArticleId(2);
         paragraphs.forEach(System.out::println);
     }
+
+    @Test
+    public void geParagraphByArticleId2() {
+        var paragraphs = paragraphService.getByArticleId(2);
+        paragraphs.forEach(System.out::println);
+    }
 }
